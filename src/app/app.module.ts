32caa978d7055f19c9c31bfe22f22acc8b7b3a2b
huangyulin <yulin.huang@zzjunzhi.com>
2020-04-30 17:19:25 +0800

    feat:组件添加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponent1Component } from './my-component1/my-component1.component';
+import { MyComponent2Component } from './my-component2/my-component2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      MyComponent1Component,
+      MyComponent2Component
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
